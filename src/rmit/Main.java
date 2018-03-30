6dc400a6c10c4f0ce145f650c2068c2a32b03ae3
BeAware <lkgame1993@gmail.com>
2018-03-30 23:52:09 +0700

    just do main menu and shop class

--- src/rmit/Main.java
+++ src/rmit/Main.java
@@ -1,8 +1,44 @@
 package rmit;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// i changed something here
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            menu();
+            int user_Option = scanner.nextInt();
+            if(user_Option == 1){
+                System.out.println("Customer setting");
+                break;
+            }else if(user_Option == 2){
+                System.out.println("Shop setting");
+
+            }else if(user_Option == 3){
+                System.out.println("Buying setting");
+                break;
+            }else if(user_Option == 4){
+                System.out.println("Drawing setting");
+                break;
+            }else if(user_Option == 5){
+                System.out.println("Exit Program!!");
+                break;
+            }else {
+                System.out.println("Invalid Option!!");
+            }
+
+        }
     }
+
+    private static void menu(){
+        System.out.println("1: Customer");
+        System.out.println("2: Shop");
+        System.out.println("3: Buying");
+        System.out.println("4: Drawing");
+        System.out.println("5: Exit");
+        System.out.println("Please choose options from 1 to 5: ");
+    }
+
+
 }
--- src/rmit/Shop.java
+++ src/rmit/Shop.java
@@ -1,4 +1,86 @@
 package rmit;
 
 public class Shop {
+    private int Code;
+    private String Address;
+    private String Owner;
+    private String Phone;
+    private String Email;
+    private double accountBalance;
+
+    public void add(int Code, String Address, String Owner, String Phone, String Email, double accountBalance){
+        //code
+    }
+
+    public void edit(int Code){
+        //code
+    }
+
+    public void view(int Code){
+        //code
+    }
+
+    public void delete(int Code){
+        //code
+    }
+
+    public Shop() {
+    }
+
+    public Shop(int code, String address, String owner, String phone, String email, double accountBalance) {
+        Code = code;
+        Address = address;
+        Owner = owner;
+        Phone = phone;
+        Email = email;
+        this.accountBalance = accountBalance;
+    }
+
+    public int getCode() {
+        return Code;
+    }
+
+    public void setCode(int code) {
+        Code = code;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
 }

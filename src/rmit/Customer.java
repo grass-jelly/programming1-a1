8131160e44435954aeaca4919be36c866d024915
Linh Do <33480996+grass-jelly@users.noreply.github.com>
2018-04-01 13:31:23 +0700

    main + customer console/ customer initial set-up

--- src/rmit/Customer.java
+++ src/rmit/Customer.java
@@ -1,5 +1,68 @@
 package rmit;
 
 public class Customer {
-    //changed
+    private String name;
+    private String birthdate;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Customer(String name, String birthdate, String address, String phone, String email) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/rmit/Main.java
+++ src/rmit/Main.java
@@ -1,44 +1,86 @@
 package rmit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        while(true){
-            Scanner scanner = new Scanner(System.in);
-            menu();
-            int user_Option = scanner.nextInt();
-            if(user_Option == 1){
-                System.out.println("Customer setting");
-                break;
-            }else if(user_Option == 2){
-                System.out.println("Shop setting");
-
-            }else if(user_Option == 3){
-                System.out.println("Buying setting");
-                break;
-            }else if(user_Option == 4){
-                System.out.println("Drawing setting");
-                break;
-            }else if(user_Option == 5){
-                System.out.println("Exit Program!!");
-                break;
-            }else {
-                System.out.println("Invalid Option!!");
+        List<Customer> customerList = new ArrayList<Customer>();
+        String userOption = getUserOption("mainMenu");
+        while (!userOption.equals("5")) {
+            switch (userOption) {
+                case "1":
+                    System.out.println("Customers setting");
+                    customer(customerList);
+                    break;
+                case "2":
+                    System.out.println("Shops setting");
+                    break;
+                case "3":
+                    System.out.println("Buying setting");
+                    break;
+                case "4":
+                    System.out.println("Drawing setting");
+                    break;
             }
-
+            userOption = getUserOption("mainMenu");
         }
     }
 
-    private static void menu(){
-        System.out.println("1: Customer");
-        System.out.println("2: Shop");
-        System.out.println("3: Buying");
-        System.out.println("4: Drawing");
-        System.out.println("5: Exit");
-        System.out.println("Please choose options from 1 to 5: ");
+    private static void customer(List<Customer> customerList) {
+        String userOption = getUserOption("customerSetting");
+
+        while (!userOption.equals("5")) {
+            switch (userOption) {
+                case "1":
+                    System.out.println("1");
+                    break;
+                case "2":
+                    System.out.println("2");
+                    break;
+                case "3":
+                    System.out.println("3");
+                    break;
+                case "4":
+                    System.out.println("4");
+                    break;
+            }
+            userOption = getUserOption("customerSetting");
+        }
     }
 
+    private static String getUserOption(String setting) {
+        if (setting.equals("mainMenu")) {
+            System.out.println("Main Menu");
+            System.out.println("1: Customers");
+            System.out.println("2: Shops");
+            System.out.println("3: Buying");
+            System.out.println("4: Drawing");
+            System.out.println("5: Exit");
+        } else {
+            System.out.println("1. Add");
+            System.out.println("2. Edit");
+            System.out.println("3. Delete");
+            System.out.println("4. View");
+            System.out.println("5. Return to Main Menu");
+        }
+
+
+        String[] options = {"1", "2", "3", "4", "5"};
+        Scanner sc = new Scanner(System.in);
+        String userOption=null;
+        boolean valid = false;
+        while (!valid) {
+            System.out.print("Please choose an option (1-5): ");
+            userOption = sc.nextLine();
+            if (!Arrays.asList(options).contains(userOption))
+                System.out.println("Invalid option!");
+            else valid = true;
+        }
+        return userOption;
+    }
 
 }
